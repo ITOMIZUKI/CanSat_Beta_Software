7858791f934efbb0339343d3629b7352142fbc79
jjj999 <spaceship2021@gmail.com>
2020-02-29 21:58:01 +0900

    arranged sensor.py

--- sensors/sensors.py
+++ sensors/sensors.py
@@ -1,38 +1,42 @@
 from time import time, sleep
-import threading
 import math
 import sys
 
 from smbus import SMBus 
 
 import serial
-from micropyGPS import MicropyGPS
+from exlib.micropyGPS import MicropyGPS
 
 
 class Logger:
 
-    def __init__(self, classes, serial=False):
+    def __init__(self, classes:dict):
 
         self.classes = classes
 
         # make instances
-        self.sensor = {}
+        self.sensors = {}
         for key, clss in self.classes.items():
-            self.sensor[key] = clss(key)
+            self.sensors[key] = clss(key)
 
-    def read(self)->"return data of all sensors as a dictionary that has the address or serial port of a sensor and its value":
+    def read(self)->"return data of all sensors as dict":
+        """
+        this method returns data of all sensors as a dict type.
+        the returned data has keys of addresses or serial port names.
+        """
         
         # read data using each instance method of read()
+        # key: name of data, value: data
         data = {}
-        for key, instance in self.sensor.items():
-            data[key] = instance.read()
+        for instance in self.sensors.values():
+            data.update(instance.read(name=True))
 
         return data
 
 
 class Bme280:
 
-    def __init__(self, address, bus_number=1):
+    def __init__(self, address=0x76, bus_number=1):
         """
         address: address of bme280
         bus_number: 
@@ -40,6 +44,7 @@ class Bme280:
     
         self.bus = SMBus(bus_number)
         self.address = address
+        self.data_name = "press", "temp", "humid"
 
         self.digT = []
         self.digP = []
@@ -92,7 +97,52 @@ class Bme280:
             if self.digH[i] & 0x8000:
                 self.digH[i] = (-self.digH[i] ^ 0xFFFF) + 1  
 
-    def compensate_P(self, adc_P):
+    def writeReg(self, reg_address, data):
+        self.bus.write_byte_data(self.address, reg_address, data)
+
+    def setup(self):
+        osrs_t = 1			#Temperature oversampling x 
+        osrs_p = 1			#Pressure oversampling x 1
+        osrs_h = 1			#Humidity oversampling x 1
+        mode   = 3			#Normal mode
+        t_sb   = 5			#Tstandby 1000ms
+        filter = 0			#Filter off
+        spi3w_en = 0			#3-wire SPI Disable
+
+        ctrl_meas_reg = (osrs_t << 5) | (osrs_p << 2) | mode
+        config_reg    = (t_sb << 5) | (filter << 2) | spi3w_en
+        ctrl_hum_reg  = osrs_h
+
+        self.writeReg(0xF2, ctrl_hum_reg)
+        self.writeReg(0xF4, ctrl_meas_reg)
+        self.writeReg(0xF5, config_reg)
+
+    # methods for reading sensors's data
+    def read(self, name=False)->"return sensor data, (press, temp, humid)":
+        """
+        name: False -> only return tuple with data
+              True  -> return dictionary with data and its name
+        """
+
+        if name:
+            return self._read_with_name
+        else:
+            return self._read_no_name
+
+    def _read_no_name(self):
+        data = []
+        for i in range (0xF7, 0xF7+8):
+            data.append(self.bus.read_byte_data(self.address, i))
+        pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
+        temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
+        hum_raw  = (data[6] << 8)  |  data[7]
+
+        return self.read_press(pres_raw), self.read_temp(temp_raw), self.read_humid(hum_raw)
+
+    def _read_with_name(self):
+        return dict(self.address, self._read_no_name)
+
+    def read_press(self, adc_P):
         self.t_fine
         pressure = 0.0
 
@@ -116,7 +166,7 @@ class Bme280:
 
         return pressure/100
 
-    def compensate_T(self, adc_T):
+    def read_temp(self, adc_T):
         self.t_fine
         v1 = (adc_T / 16384.0 - self.digT[0] / 1024.0) * self.digT[1]
         v2 = (adc_T / 131072.0 - self.digT[0] / 8192.0) * (adc_T / 131072.0 - self.digT[0] / 8192.0) * self.digT[2]
@@ -124,7 +174,7 @@ class Bme280:
         temperature = self.t_fine / 5120.0
         return temperature 
 
-    def compensate_H(self, adc_H):
+    def read_humid(self, adc_H):
         self.t_fine
         var_h = self.t_fine - 76800.0
         if var_h != 0:
@@ -137,47 +187,6 @@ class Bme280:
         elif var_h < 0.0:
             var_h = 0.0
         return var_h
-
-    def writeReg(self, reg_address, data):
-        self.bus.write_byte_data(self.address, reg_address, data)
-
-    def setup(self):
-        osrs_t = 1			#Temperature oversampling x 
-        osrs_p = 1			#Pressure oversampling x 1
-        osrs_h = 1			#Humidity oversampling x 1
-        mode   = 3			#Normal mode
-        t_sb   = 5			#Tstandby 1000ms
-        filter = 0			#Filter off
-        spi3w_en = 0			#3-wire SPI Disable
-
-        ctrl_meas_reg = (osrs_t << 5) | (osrs_p << 2) | mode
-        config_reg    = (t_sb << 5) | (filter << 2) | spi3w_en
-        ctrl_hum_reg  = osrs_h
-
-        self.writeReg(0xF2, ctrl_hum_reg)
-        self.writeReg(0xF4, ctrl_meas_reg)
-        self.writeReg(0xF5, config_reg)
-
-    def read(self, name=False)->"return sensor data, (press, temp, humid)":
-        """
-        name: False -> only return tuple with data
-              True  -> return dictionary with data and its name
-        """
-        data = []
-        for i in range (0xF7, 0xF7+8):
-            data.append(self.bus.read_byte_data(self.address,i))
-        pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
-        temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
-        hum_raw  = (data[6] << 8)  |  data[7]
-
-        data = self.compensate_P(pres_raw), self.compensate_T(temp_raw), self.compensate_H(hum_raw)
-
-        if name == True:
-            data_name = "press", "temp", "humid"
-            data = dict(data_name, data)
-            return data
-        else:
-            return data
 
 
 class Apds9301:
@@ -211,10 +220,12 @@ class Apds9301:
     _gain = 0
     _integration = 0
 
-    def __init__(self, address, bus_number=1):
+    def __init__(self, address=0x39, bus_number=1):
 
         self.bus = SMBus(bus_number)
         self.address = address
+        self.data_name = "lux", 
+
         self.set_power(True)
         self.set_timing(True, 0)
 
@@ -244,12 +255,19 @@ class Apds9301:
             Apds9301._integration = Apds9301.REG_TIMING_SCALE_402MS
         self.bus.write_byte_data(self.address, Apds9301.REG_TIMING | Apds9301.REG_CONTROL_CMD, regval) #init
 
-    def read_raw(self):
+    # methods for reading sensor's data
+    def read(self, name=False):
+
+        if name:
+            return dict(self.data_name, (self.read_lux, ))
+        else:
+            return self.read_lux, 
+
+    def read_lux(self):
+
         ch0 = self.bus.read_word_data(self.address, Apds9301.REG_CONTROL_CMD | Apds9301.REG_CONTROL_WORD | Apds9301.REG_DATA0LOW) # 0xAC
         ch1 = self.bus.read_word_data(self.address, Apds9301.REG_CONTROL_CMD | Apds9301.REG_CONTROL_WORD | Apds9301.REG_DATA1LOW) # 0xAE
-        return [ch0, ch1]
 
-    def calc_lux(self, ch0, ch1):
         ch0 = ch0 / Apds9301._gain / Apds9301._integration
         ch1 = ch1 / Apds9301._gain / Apds9301._integration
         if ch0 == 0:
@@ -272,12 +290,8 @@ class Apds9301:
             lux = 0
         return lux
 
-    def read(self):
-        ch0, ch1 = self.read_raw()
-        return (self.calc_lux(ch0, ch1),)
 
-
-class SL_MPU9250:
+class Mpu9250:
     # Constant definition
     REG_PWR_MGMT_1      = 0x6B
     REG_INT_PIN_CFG     = 0x37
@@ -310,11 +324,12 @@ class SL_MPU9250:
     offsetGyroZ         = 0.0
 
     # Constructor
-    def __init__(self, address, channel):
+    def __init__(self, address=0x68, bus_number=1):
         self.address    = address
-        self.channel    = channel
-        self.bus        = SMBus(self.channel)
+        self.bus        = SMBus(bus_number)
         self.addrAK8963 = 0x0C
+
+        self.data_name = "acc_x", "acc_y", "acc_z", "gyr_x", "gyr_y", "gyr_z", "mag_x", "mag_y", "mag_z"
 
         # Sensor initialization
         self.resetRegister()
@@ -444,29 +459,101 @@ class SL_MPU9250:
     # Function to set LowPassFilter of acceleration sensor.
     # def setAccelLowPassFilter(self,val):      
 
-    #The data from the sensor is treated as unsigned. So, converted to signed. (limited to 16 bits)
-    def u2s(self,unsigneddata):
-        if unsigneddata & (0x01 << 15) : 
-            return -1 * ((unsigneddata ^ 0xffff) + 1)
-        return unsigneddata
+    def selfTestMag(self):
+        print ("start mag sensor self test")
+        self.setMagRegister('SELF_TEST','16bit')
+        self.bus.write_i2c_block_data(self.addrAK8963, 0x0C, [0x40])
+        sleep(1.0)
+        data = self.read_mag()
 
-    # Function to obtain acceleration value.
-    def getAccel(self):
+        print (data)
+
+        self.bus.write_i2c_block_data(self.addrAK8963, 0x0C, [0x00])
+        self.setMagRegister('POWER_DOWN','16bit')
+        sleep(1.0)
+        print ("end mag sensor self test")
+        return
+
+    # Calibrate the acceleration sensor
+    # I think that you really need to consider latitude, altitude, terrain, etc., but I briefly thought about it.
+    # It is a premise that gravity is correctly applied in the direction of the z axis and acceleration other than gravity is not generated.
+    def calibAccel(self, _count=1000):
+        print ("Accel calibration start")
+        _sum    = [0,0,0]
+
+        # get data sample.
+        for _i in range(_count):
+            _data   = self.read_acc()
+            _sum[0] += _data[0]
+            _sum[1] += _data[1]
+            _sum[2] += _data[2]
+
+        # Make the average value an offset.
+        self.offsetAccelX   = -1.0 * _sum[0] / _count
+        self.offsetAccelY   = -1.0 * _sum[1] / _count
+        self.offsetAccelZ   = -1.0 * ((_sum[2] / _count ) - 1.0)    # 重力分を差し引く
+
+        # I want to register an offset value in a register. But I do not know the behavior, so I will put it on hold.
+
+        print ("Accel calibration complete")
+        return self.offsetAccelX, self.offsetAccelY, self.offsetAccelZ
+
+    # Calibrate the gyro sensor
+    # Assumption that no rotation occurs on each axis
+    def calibGyro(self, _count=1000):
+        print ("Gyro calibration start")
+        _sum    = [0,0,0]
+
+        # get data sample
+        for _i in range(_count):
+            _data   = self.read_gyro()
+            _sum[0] += _data[0]
+            _sum[1] += _data[1]
+            _sum[2] += _data[2]
+
+        # Make the average value an offset.
+        self.offsetGyroX    = -1.0 * _sum[0] / _count
+        self.offsetGyroY    = -1.0 * _sum[1] / _count
+        self.offsetGyroZ    = -1.0 * _sum[2] / _count
+
+        # I want to register an offset value in a register. But I do not know the behavior, so I will put it on hold.
+
+        print ("Gyro calibration complete")
+        return self.offsetGyroX, self.offsetGyroY, self.offsetGyroZ
+
+    # methods for reading sensor's data
+    def read(self, name=False)->"return data depending on the option 'name'":
+
+        if name:
+            return self._read_with_name
+        else:
+            return self._read_no_name
+
+    def _read_no_name(self)->"return sensor data as tuple without name":
+        acc = self.read_acc()
+        gyr = self.read_gyro()
+        mag = self.read_mag()
+        data = tuple([obj[i] for obj in [acc, gyr, mag] for i in range(3)])
+        return data
+
+    def _read_with_name(self)->"return sensor data as dict with names":
+        return dict(self.data_name, self._read_no_name)
+
+    def read_acc(self):
         data    = self.bus.read_i2c_block_data(self.address, 0x3B ,6)
         rawX    = self.accelCoefficient * self.u2s(data[0] << 8 | data[1]) + self.offsetAccelX
         rawY    = self.accelCoefficient * self.u2s(data[2] << 8 | data[3]) + self.offsetAccelY
         rawZ    = self.accelCoefficient * self.u2s(data[4] << 8 | data[5]) + self.offsetAccelZ
         return rawX, rawY, rawZ
 
-    # Function to obtain gyro value.
-    def getGyro(self):
+    def read_gyro(self):
         data    = self.bus.read_i2c_block_data(self.address, 0x43 ,6)
         rawX    = self.gyroCoefficient * self.u2s(data[0] << 8 | data[1]) + self.offsetGyroX
         rawY    = self.gyroCoefficient * self.u2s(data[2] << 8 | data[3]) + self.offsetGyroY
         rawZ    = self.gyroCoefficient * self.u2s(data[4] << 8 | data[5]) + self.offsetGyroZ
         return rawX, rawY, rawZ
 
-    def getMag(self):
+    def read_mag(self):
         if self.MAG_ACCESS == False:
             # Magnetometric sensor is disable.
             raise Exception('002 Access to a sensor is invalid.')
@@ -525,123 +612,68 @@ class SL_MPU9250:
 
         return rawX, rawY, rawZ
 
-    def getTemp(self):
+    def read_temp(self):
         data    = self.bus.read_i2c_block_data(self.address, 0x65 ,2)
         raw     = data[0] << 8 | data[1]
         return ((raw - self.offsetRoomTemp) / self.tempSensitivity) + 21
 
-    def selfTestMag(self):
-        print ("start mag sensor self test")
-        self.setMagRegister('SELF_TEST','16bit')
-        self.bus.write_i2c_block_data(self.addrAK8963, 0x0C, [0x40])
-        sleep(1.0)
-        data = self.getMag()
-
-        print (data)
-
-        self.bus.write_i2c_block_data(self.addrAK8963, 0x0C, [0x00])
-        self.setMagRegister('POWER_DOWN','16bit')
-        sleep(1.0)
-        print ("end mag sensor self test")
-        return
-
-    # Calibrate the acceleration sensor
-    # I think that you really need to consider latitude, altitude, terrain, etc., but I briefly thought about it.
-    # It is a premise that gravity is correctly applied in the direction of the z axis and acceleration other than gravity is not generated.
-    def calibAccel(self, _count=1000):
-        print ("Accel calibration start")
-        _sum    = [0,0,0]
-
-        # get data sample.
-        for _i in range(_count):
-            _data   = self.getAccel()
-            _sum[0] += _data[0]
-            _sum[1] += _data[1]
-            _sum[2] += _data[2]
-
-        # Make the average value an offset.
-        self.offsetAccelX   = -1.0 * _sum[0] / _count
-        self.offsetAccelY   = -1.0 * _sum[1] / _count
-        self.offsetAccelZ   = -1.0 * ((_sum[2] / _count ) - 1.0)    # 重力分を差し引く
-
-        # I want to register an offset value in a register. But I do not know the behavior, so I will put it on hold.
-
-        print ("Accel calibration complete")
-        return self.offsetAccelX, self.offsetAccelY, self.offsetAccelZ
-
-    # Calibrate the gyro sensor
-    # Assumption that no rotation occurs on each axis
-    def calibGyro(self, _count=1000):
-        print ("Gyro calibration start")
-        _sum    = [0,0,0]
-
-        # get data sample
-        for _i in range(_count):
-            _data   = self.getGyro()
-            _sum[0] += _data[0]
-            _sum[1] += _data[1]
-            _sum[2] += _data[2]
-
-        # Make the average value an offset.
-        self.offsetGyroX    = -1.0 * _sum[0] / _count
-        self.offsetGyroY    = -1.0 * _sum[1] / _count
-        self.offsetGyroZ    = -1.0 * _sum[2] / _count
-
-        # I want to register an offset value in a register. But I do not know the behavior, so I will put it on hold.
-
-        print ("Gyro calibration complete")
-        return self.offsetGyroX, self.offsetGyroY, self.offsetGyroZ
-
-
-    def read(self, name=False)->"return sensor data, (acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z, mag_x, mag_y, mag_z)":
-            '''
-            name: False ->only return tuple with data
-                  True  ->return dictionary with data and its name
-            '''
-            
-            acc = self.getAccel()
-            gyr = self.getGyro()
-            mag = self.getMag()
-
-            data = acc[0], acc[1], acc[2], gyr[0], gyr[1], gyr[2], mag[0] ,mag[1] ,mag[2]
-
-            if name == True:
-                data_name = "acc_x", "acc_y", "acc_z", "gyr_x", "gyr_y", "gyr_z", "mag_x", "mag_y", "mag_z"
-                data = dict(data_name, data)
-                return data
-            else:
-                return data
+    #The data from the sensor is treated as unsigned. So, converted to signed. (limited to 16 bits)
+    def u2s(self,unsigneddata):
+        if unsigneddata & (0x01 << 15) : 
+            return -1 * ((unsigneddata ^ 0xffff) + 1)
+        return unsigneddata
 
 
 class Gps:
 
-    def __init__(self, serial_port, timeout=10):
+    def __init__(self, serial_port:str, timeout:int=10):
+        """
+        serial_port: path of a usb port used for GPS module
+        timeout: time for timeout 
+        """
         self.gps_serial = serial.Serial(serial_port, 9600, timeout=timeout)
         self.gps = MicropyGPS(9, "dd")
+        self.data_name = "lati", "longi"
         
         self.setup()
 
 
-    def setup(self):
-        
+    def setup(self):     
         while self.read() == (0, 0):
-            pass
-            sleep(1)
+            sleep(0.5)
         
+    def read(self, name=False):
 
-    def read(self):
-        sentence = self.gps_serial.readline().decode("utf-8")
-        
+        if name:
+            return self._read_with_name
+        else:
+            return self._read_no_name
+
+    def _read_no_name(self):
+
+        # for unicode decode error at the first reading
+        while True:
+            try:
+                sentence = self.gps_serial.readline().decode("utf-8")
+                break
+            except UnicodeDecodeError:
+                pass
+
         for x in sentence:
             self.gps.update(x)
 
-        return round(self.gps.latitude[0], 2), round(self.gps.longitude[0], 2)
+        return self.gps.latitude[0], self.gps.longitude[0]
+
+    def _read_with_name(self):
+        return dict(self.data_name, self._read_no_name)
 
 
 
 if __name__ == "__main__":
-    gps = Gps("/dev/ttyUSB0")
     
+    sensors = {0x76: Bme280, 0x39: Apds9301, 0x68: Mpu9250, "/dev/ttyUSB0": Gps}
+    logger = Logger(sensors)
+
     while True:
-        print(gps.read())
+        print("data:", logger.read())
         sleep(1)
