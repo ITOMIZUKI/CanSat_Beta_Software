e1050ed9a4b806ef50971461be2146e979ea587f
ITOMIZUKI <44338865+ITOMIZUKI@users.noreply.github.com>
2020-03-04 19:13:11 +0900

    Update log.py

--- sensors/log.py
+++ sensors/log.py
@@ -1,7 +1,52 @@
 import numpy as np
 import pandas as pd
 
+class Log:import time
+import datetime
+import numpy as np
+import pandas as pd
+
 class Log:
+    """
+    df_log have all data and timestamp
+    'acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'humid', 'lati', 'longi', 'lux', 'mag_x', 'mag_y', 'mag_z', 'press', 'temp', 'timestamp'
+    """
+
+    def __init__(self):
+        keys = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'humid', 'lati', 'longi', 'lux', 'mag_x', 'mag_y', 'mag_z', 'press', 'temp']
+        #keys = data.keys() #get index of data
+        self.df_log = pd.DataFrame(columns=keys)
+        self.df_log.to_csv('datalog.csv')
+
+    def adddata(self, data):#add current data to Dataframe
+        """
+        data: dict, holding datas of sensors
+        ---------------------------------------------
+        add last data to dataframe and write to csv file
+        you should pass dict with same columns 
+        """
+        dt_now = str(datetime.datetime.now())
+        dict_time = {'timestamp': dt_now}
+        data.update(dict_time)
+        series = {}
+        for k, v in data.items():
+            series[k] = pd.Series(v)
+        line = pd.DataFrame(series)
+
+        self.df_log = pd.concat([self.df_log, line])
+        self.df_log.to_csv('datalog.csv')
+
+if __name__ == '__main__':
+
+    keys = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'humid', 'lati', 'longi', 'lux', 'mag_x', 'mag_y', 'mag_z', 'press', 'temp']
+    values = [0 for _ in range(15)]
+    d = dict(zip(keys, values))    #dammy data, have keys and zeros index
+
+    log = Log()
+    for i in range(6):
+        log.adddata(d)
+        #add zeros data five times
+    print (log.df_log)
 
     def __init__(self):
         keys = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'humid', 'lati', 'longi', 'lux', 'mag_x', 'mag_y', 'mag_z', 'press', 'temp']
@@ -23,16 +68,3 @@ class Log:
 
         self.df = pd.concat([self.df, line])
         self.df.to_csv('datalog.csv', index = True)
-
-if __name__ == '__main__':
-
-    keys = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'humid', 'lati', 'longi', 'lux', 'mag_x', 'mag_y', 'mag_z', 'press', 'temp']
-    values = [0 for _ in range(15)]
-    d = dict(zip(keys, values))
-    #dammy data, have keys and zeros index
-
-    log = Log()
-    for i in range(6):
-        log.adddata(d)
-        #add zeros data five times
-    print (log.df)
