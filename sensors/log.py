9cf7a6463df7e3810f12a1404f6d60286fb8bf34
ITOMIZUKI <44338865+ITOMIZUKI@users.noreply.github.com>
2020-03-08 15:58:11 +0900

    Update log.py

--- sensors/log.py
+++ sensors/log.py
@@ -1,70 +1,61 @@
-import numpy as np
-import pandas as pd
-
-class Log:import time
+import time
 import datetime
 import numpy as np
 import pandas as pd
 
+import random
+
 class Log:
     """
-    df_log have all data and timestamp
-    'acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'humid', 'lati', 'longi', 'lux', 'mag_x', 'mag_y', 'mag_z', 'press', 'temp', 'timestamp'
+    Log().df is DataFrame
+    pass name to write in
+    then write data in need
     """
 
-    def __init__(self):
-        keys = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'humid', 'lati', 'longi', 'lux', 'mag_x', 'mag_y', 'mag_z', 'press', 'temp']
-        #keys = data.keys() #get index of data
-        self.df_log = pd.DataFrame(columns=keys)
-        self.df_log.to_csv('datalog.csv')
+    def __init__(self, name):
 
-    def adddata(self, data):#add current data to Dataframe
+        #keys = data.keys() #get index of data
+        #items = data.items() #get all cells of data
+
+        self.df = pd.DataFrame()
+        self.name = name
+        self.df.to_csv(self.name)
+
+    def write(self, data, time=False):#add current data to Dataframe
         """
         data: dict, holding datas of sensors
         ---------------------------------------------
-        add last data to dataframe and write to csv file
-        you should pass dict with same columns 
+        add data to hip dataframe and write whole data to csv file
+        pass dict which have same columns mentioned before
+        can add time if time is True
         """
-        dt_now = str(datetime.datetime.now())
-        dict_time = {'timestamp': dt_now}
-        data.update(dict_time)
+
+        if (time == True):
+            dt_now = str(datetime.datetime.now())
+            dict_time = {'timestamp': dt_now}
+            data.update(dict_time)
+
         series = {}
         for k, v in data.items():
             series[k] = pd.Series(v)
         line = pd.DataFrame(series)
 
-        self.df_log = pd.concat([self.df_log, line])
-        self.df_log.to_csv('datalog.csv')
+        self.df = self.df.append(line, ignore_index = True)
+        self.df.to_csv(self.name)
+
+    def demodata(self):    #dammy data, have keys and random values
+        keys = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'humid', 'lati', 'longi', 'lux', 'mag_x', 'mag_y', 'mag_z', 'press', 'temp']
+        values = [random.random() for _ in range(15)]
+        self.d = dict(zip(keys, values))
 
 if __name__ == '__main__':
+    log = Log('log.csv')
+    ctrl = Log('ctrl.csv')
 
-    keys = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'humid', 'lati', 'longi', 'lux', 'mag_x', 'mag_y', 'mag_z', 'press', 'temp']
-    values = [0 for _ in range(15)]
-    d = dict(zip(keys, values))    #dammy data, have keys and zeros index
-
-    log = Log()
-    for i in range(6):
-        log.adddata(d)
+    for i in range(5):
+        log.demodata()
+        ctrl.demodata()
+        
+        log.write(log.d)
+        ctrl.write(ctrl.d, time=True)
         #add zeros data five times
-    print (log.df_log)
-
-    def __init__(self):
-        keys = ['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'humid', 'lati', 'longi', 'lux', 'mag_x', 'mag_y', 'mag_z', 'press', 'temp']
-        #keys = rawdata.keys() #get index of data
-        self.df = pd.DataFrame(columns=keys)
-        self.df.to_csv('datalog.csv')
-
-    def adddata(self, rawdata):#add current data to Dataframe
-        """
-        rawdata: dict, holding datas of sensors
-        ---------------------------------------------
-        add last data to dataframe and write to csv file
-        you should pass dict with same columns    
-        """
-        series = {}
-        for k, v in rawdata.items():
-            series[k] = pd.Series(v)
-        line = pd.DataFrame(series)
-
-        self.df = pd.concat([self.df, line])
-        self.df.to_csv('datalog.csv', index = True)
